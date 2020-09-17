a36b751ba8b7150bbf9160c4873985f2e25826fa
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2020-09-17 18:48:07 +0700

    intial

---
+++ precache-manifest.29a585cb66051ea307d41e2baa66d32d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0ee86d39641d3a2a68252e4fb8c1ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe5bb70a966b1afa7678",
+    "url": "/static/js/2.29b260bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.29b260bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41664e9b2de1165130e1",
+    "url": "/static/js/main.c6d0d742.chunk.js"
+  },
+  {
+    "revision": "4040b55ff6b868e876aa",
+    "url": "/static/js/runtime-main.2c219ed3.js"
+  }
+]);
